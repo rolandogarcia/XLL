4382ce9c0f2c4a4aa95faf725ab5603090a47de6
rolandogarcia <rolando.garcia@infosel.com>
2017-02-06 23:19:17 -0600

    Commit 01

---
+++ InfoselXLLAddin/XLLModule.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace InfoselXLLAddin
+{
+    partial class XLLModule
+    {
+        /// <summary>
+        /// Required by designer
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+ 
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+        /// <summary>
+        /// Required by designer support - do not modify
+        /// the following method
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.adxExcelFunctionCategory1 = new AddinExpress.MSO.ADXExcelFunctionCategory(this.components);
+            this.adxExcelFunctionDescriptor1 = new AddinExpress.MSO.ADXExcelFunctionDescriptor(this.components);
+            this.adxExcelParameterDescriptor1 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelFunctionDescriptor2 = new AddinExpress.MSO.ADXExcelFunctionDescriptor(this.components);
+            this.adxExcelFunctionDescriptor3 = new AddinExpress.MSO.ADXExcelFunctionDescriptor(this.components);
+            this.adxExcelFunctionDescriptor4 = new AddinExpress.MSO.ADXExcelFunctionDescriptor(this.components);
+            this.adxExcelFunctionDescriptor5 = new AddinExpress.MSO.ADXExcelFunctionDescriptor(this.components);
+            this.adxExcelParameterDescriptor2 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelParameterDescriptor3 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelParameterDescriptor4 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelParameterDescriptor5 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelParameterDescriptor6 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelParameterDescriptor7 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelParameterDescriptor8 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelParameterDescriptor9 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelFunctionDescriptor7 = new AddinExpress.MSO.ADXExcelFunctionDescriptor(this.components);
+            this.adxExcelParameterDescriptor12 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelParameterDescriptor13 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelFunctionDescriptor6 = new AddinExpress.MSO.ADXExcelFunctionDescriptor(this.components);
+            this.adxExcelParameterDescriptor10 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelParameterDescriptor11 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelFunctionDescriptor8 = new AddinExpress.MSO.ADXExcelFunctionDescriptor(this.components);
+            this.adxExcelParameterDescriptor14 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            this.adxExcelFunctionDescriptor9 = new AddinExpress.MSO.ADXExcelFunctionDescriptor(this.components);
+            this.adxExcelParameterDescriptor15 = new AddinExpress.MSO.ADXExcelParameterDescriptor(this.components);
+            // 
+            // adxExcelFunctionCategory1
+            // 
+            this.adxExcelFunctionCategory1.CategoryName = "Infosel";
+            this.adxExcelFunctionCategory1.FunctionDescriptors.Add(this.adxExcelFunctionDescriptor2);
+            this.adxExcelFunctionCategory1.FunctionDescriptors.Add(this.adxExcelFunctionDescriptor3);
+            this.adxExcelFunctionCategory1.FunctionDescriptors.Add(this.adxExcelFunctionDescriptor4);
+            this.adxExcelFunctionCategory1.FunctionDescriptors.Add(this.adxExcelFunctionDescriptor5);
+            this.adxExcelFunctionCategory1.FunctionDescriptors.Add(this.adxExcelFunctionDescriptor1);
+            this.adxExcelFunctionCategory1.FunctionDescriptors.Add(this.adxExcelFunctionDescriptor6);
+            this.adxExcelFunctionCategory1.FunctionDescriptors.Add(this.adxExcelFunctionDescriptor7);
+            this.adxExcelFunctionCategory1.FunctionDescriptors.Add(this.adxExcelFunctionDescriptor8);
+            this.adxExcelFunctionCategory1.FunctionDescriptors.Add(this.adxExcelFunctionDescriptor9);
+            // 
+            // adxExcelFunctionDescriptor1
+            // 
+            this.adxExcelFunctionDescriptor1.FunctionName = "IQL_Cuadrado";
+            this.adxExcelFunctionDescriptor1.ParameterDescriptors.Add(this.adxExcelParameterDescriptor1);
+            // 
+            // adxExcelParameterDescriptor1
+            // 
+            this.adxExcelParameterDescriptor1.ParameterName = "a";
+            // 
+            // adxExcelFunctionDescriptor2
+            // 
+            this.adxExcelFunctionDescriptor2.FunctionName = "IQL_Suma";
+            this.adxExcelFunctionDescriptor2.ParameterDescriptors.Add(this.adxExcelParameterDescriptor2);
+            this.adxExcelFunctionDescriptor2.ParameterDescriptors.Add(this.adxExcelParameterDescriptor3);
+            // 
+            // adxExcelFunctionDescriptor3
+            // 
+            this.adxExcelFunctionDescriptor3.FunctionName = "IQL_Resta";
+            this.adxExcelFunctionDescriptor3.ParameterDescriptors.Add(this.adxExcelParameterDescriptor4);
+            this.adxExcelFunctionDescriptor3.ParameterDescriptors.Add(this.adxExcelParameterDescriptor5);
+            // 
+            // adxExcelFunctionDescriptor4
+            // 
+            this.adxExcelFunctionDescriptor4.FunctionName = "IQL_Multiplica";
+            this.adxExcelFunctionDescriptor4.ParameterDescriptors.Add(this.adxExcelParameterDescriptor6);
+            this.adxExcelFunctionDescriptor4.ParameterDescriptors.Add(this.adxExcelParameterDescriptor7);
+            // 
+            // adxExcelFunctionDescriptor5
+            // 
+            this.adxExcelFunctionDescriptor5.FunctionName = "IQL_Divide";
+            this.adxExcelFunctionDescriptor5.ParameterDescriptors.Add(this.adxExcelParameterDescriptor8);
+            this.adxExcelFunctionDescriptor5.ParameterDescriptors.Add(this.adxExcelParameterDescriptor9);
+            // 
+            // adxExcelParameterDescriptor2
+            // 
+            this.adxExcelParameterDescriptor2.ParameterName = "a";
+            // 
+            // adxExcelParameterDescriptor3
+            // 
+            this.adxExcelParameterDescriptor3.ParameterName = "b";
+            // 
+            // adxExcelParameterDescriptor4
+            // 
+            this.adxExcelParameterDescriptor4.ParameterName = "a";
+            // 
+            // adxExcelParameterDescriptor5
+            // 
+            this.adxExcelParameterDescriptor5.ParameterName = "b";
+            // 
+            // adxExcelParameterDescriptor6
+            // 
+            this.adxExcelParameterDescriptor6.ParameterName = "a";
+            // 
+            // adxExcelParameterDescriptor7
+            // 
+            this.adxExcelParameterDescriptor7.ParameterName = "b";
+            // 
+            // adxExcelParameterDescriptor8
+            // 
+            this.adxExcelParameterDescriptor8.ParameterName = "a";
+            // 
+            // adxExcelParameterDescriptor9
+            // 
+            this.adxExcelParameterDescriptor9.ParameterName = "b";
+            // 
+            // adxExcelFunctionDescriptor7
+            // 
+            this.adxExcelFunctionDescriptor7.FunctionName = "IQL_ArregloMas";
+            this.adxExcelFunctionDescriptor7.ParameterDescriptors.Add(this.adxExcelParameterDescriptor12);
+            this.adxExcelFunctionDescriptor7.ParameterDescriptors.Add(this.adxExcelParameterDescriptor13);
+            // 
+            // adxExcelParameterDescriptor12
+            // 
+            this.adxExcelParameterDescriptor12.ParameterName = "array";
+            // 
+            // adxExcelParameterDescriptor13
+            // 
+            this.adxExcelParameterDescriptor13.ParameterName = "numero";
+            // 
+            // adxExcelFunctionDescriptor6
+            // 
+            this.adxExcelFunctionDescriptor6.FunctionName = "IQL_Arreglo";
+            this.adxExcelFunctionDescriptor6.ParameterDescriptors.Add(this.adxExcelParameterDescriptor10);
+            this.adxExcelFunctionDescriptor6.ParameterDescriptors.Add(this.adxExcelParameterDescriptor11);
+            // 
+            // adxExcelParameterDescriptor10
+            // 
+            this.adxExcelParameterDescriptor10.ParameterName = "rows";
+            // 
+            // adxExcelParameterDescriptor11
+            // 
+            this.adxExcelParameterDescriptor11.ParameterName = "columns";
+            // 
+            // adxExcelFunctionDescriptor8
+            // 
+            this.adxExcelFunctionDescriptor8.FunctionName = "IQL_GetLast";
+            this.adxExcelFunctionDescriptor8.ParameterDescriptors.Add(this.adxExcelParameterDescriptor14);
+            // 
+            // adxExcelParameterDescriptor14
+            // 
+            this.adxExcelParameterDescriptor14.ParameterName = "sSimbolo";
+            // 
+            // adxExcelFunctionDescriptor9
+            // 
+            this.adxExcelFunctionDescriptor9.FunctionName = "IQL_GetLast2";
+            this.adxExcelFunctionDescriptor9.ParameterDescriptors.Add(this.adxExcelParameterDescriptor15);
+            // 
+            // adxExcelParameterDescriptor15
+            // 
+            this.adxExcelParameterDescriptor15.ParameterName = "Simbolo";
+            // 
+            // XLLModule
+            // 
+            this.AddinName = "InfoselXLLAddin";
+
+        }
+        #endregion
+
+        private AddinExpress.MSO.ADXExcelFunctionCategory adxExcelFunctionCategory1;
+        private AddinExpress.MSO.ADXExcelFunctionDescriptor adxExcelFunctionDescriptor1;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor1;
+        private AddinExpress.MSO.ADXExcelFunctionDescriptor adxExcelFunctionDescriptor2;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor2;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor3;
+        private AddinExpress.MSO.ADXExcelFunctionDescriptor adxExcelFunctionDescriptor3;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor4;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor5;
+        private AddinExpress.MSO.ADXExcelFunctionDescriptor adxExcelFunctionDescriptor4;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor6;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor7;
+        private AddinExpress.MSO.ADXExcelFunctionDescriptor adxExcelFunctionDescriptor5;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor8;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor9;
+        private AddinExpress.MSO.ADXExcelFunctionDescriptor adxExcelFunctionDescriptor7;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor12;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor13;
+        private AddinExpress.MSO.ADXExcelFunctionDescriptor adxExcelFunctionDescriptor6;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor10;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor11;
+        private AddinExpress.MSO.ADXExcelFunctionDescriptor adxExcelFunctionDescriptor8;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor14;
+        private AddinExpress.MSO.ADXExcelFunctionDescriptor adxExcelFunctionDescriptor9;
+        private AddinExpress.MSO.ADXExcelParameterDescriptor adxExcelParameterDescriptor15;
+    }
+}
+
---
+++ InfoselXLLAddin/XLLModule.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Windows.Forms;
+using AddinExpress.MSO;
+using System.Xml;
+using Microsoft.Office.Interop.Excel;
+
+
+
+
+
+
+namespace InfoselXLLAddin
+{
+    /// <summary>
+    ///   Add-in Express XLL Add-in Module
+    /// </summary>
+    [ComVisible(true)]
+    public partial class XLLModule : AddinExpress.MSO.ADXXLLModule
+    {
+        public XLLModule()
+        {
+           //Application.EnableVisualStyles(); RGC Comente esta linea.
+            InitializeComponent();
+            // Please add any initialization code to the OnInitialize event handler
+        }
+ 
+        #region Add-in Express automatic code
+ 
+        // Required by Add-in Express - do not modify
+        // the methods within this region
+ 
+        public override System.ComponentModel.IContainer GetContainer()
+        {
+            if (components == null)
+                components = new System.ComponentModel.Container();
+            return components;
+        }
+ 
+        [ComRegisterFunctionAttribute]
+        public static void RegisterXLL(Type t)
+        {
+            AddinExpress.MSO.ADXXLLModule.RegisterXLLInternal(t);
+        }
+ 
+        [ComUnregisterFunctionAttribute]
+        public static void UnregisterXLL(Type t)
+        {
+            AddinExpress.MSO.ADXXLLModule.UnregisterXLLInternal(t);
+        }
+ 
+        #endregion
+ 
+        public static new XLLModule CurrentInstance
+        {
+            get
+            {
+                return AddinExpress.MSO.ADXXLLModule.CurrentInstance as XLLModule;
+            }
+        }
+
+        public Excel._Application ExcelApp
+        {
+            get
+            {
+                return (HostApplication as Excel._Application);
+            }
+        }
+
+        #region Define your UDFs in this section
+ 
+        /// <summary>
+        /// The container for user-defined functions (UDFs). Every UDF is a public static (Public Shared in VB.NET) method that returns a value of any base type: string, double, integer.
+        /// </summary>
+        internal static class XLLContainer
+        {
+            /// <summary>
+            /// Required by Add-in Express. Please do not modify this method.
+            /// </summary>
+            internal static XLLModule Module
+            {
+                get
+                {
+                    return AddinExpress.MSO.ADXXLLModule.
+                        CurrentInstance as InfoselXLLAddin.XLLModule;
+                }
+            }
+ 
+            #region Sample function
+ 
+
+          
+           
+            public static int IQL_Suma(int a, int b)
+            {
+                return a + b;
+            }
+
+            public static int IQL_Resta(int a, int b)
+            {
+                return a - b;
+            }
+
+            public static int IQL_Multiplica(int a, int b)
+            {
+                return a * b;
+            }
+
+            public static int IQL_Divide(int a, int b)
+            {
+                return a / b;
+            }
+
+            public static int IQL_Cuadrado(int a)
+            {
+                return a * a;
+            }
+
+            // Seleccionar el area, despues la funcion, alimentar parametros y despues <CTRL><ALT><ENTER>
+            // El area seleccionada debe de ser mayor a los parametros de entrada.
+            public static object IQL_Arreglo(int rows, int columns)
+            {
+                object[,] result = new string[rows, columns];
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        result[i, j] = string.Format("({0},{1})", i, j);
+                    }
+                }
+
+                return result;
+            }
+
+
+            public static object IQL_ArregloMas(object[,] array, decimal numero)
+            {
+                int rows = array.GetLength(0);
+                int columns = array.GetLength(1);
+
+                object[,] result = new string[rows, columns];
+              
+                string straux = "";
+                decimal dValor = 0;
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        straux = array[i, j].ToString();
+                        decimal.TryParse(straux, out dValor);
+                        dValor += numero;
+                        result[i, j] = dValor.ToString();
+                      }
+                }
+
+                return result;
+            }
+
+
+            public static object IQL_GetLast(string Simbolo)
+            {
+                String sURL = "http://finanzasenlinea.infosel.com/wsNeural/service.asmx/GetLastBySymbol?Symbol=" + Simbolo + "&Market=BMV&SymbolType=ACCION";
+                object[,] result = new string[1, 8];
+
+                XmlDocument xdoc = new XmlDocument();
+                xdoc.Load(sURL);
+                
+                result[0, 0] = xdoc.DocumentElement.SelectNodes("/Quote")[0]["Emisora"].InnerText;
+                result[0, 1] = xdoc.DocumentElement.SelectNodes("/Quote")[0]["UltimoHecho"].InnerText;
+                result[0, 2] = xdoc.DocumentElement.SelectNodes("/Quote")[0]["ValorApertura"].InnerText;
+                result[0, 3] = xdoc.DocumentElement.SelectNodes("/Quote")[0]["MaximoDia"].InnerText;
+                result[0, 4] = xdoc.DocumentElement.SelectNodes("/Quote")[0]["MinimoDia"].InnerText;
+                result[0, 5] = xdoc.DocumentElement.SelectNodes("/Quote")[0]["VolumenAcumulado"].InnerText;
+                result[0, 6] = xdoc.DocumentElement.SelectNodes("/Quote")[0]["Fecha"].InnerText;
+                result[0, 7] = xdoc.DocumentElement.SelectNodes("/Quote")[0]["Hora"].InnerText;
+
+                return result;
+            }
+
+
+
+            public static object IQL_GetLast2(string Simbolo)
+            {                
+                return 0;            
+            }            
+
+
+            // http://stackoverflow.com/questions/17476620/returning-array-in-excel-xll
+
+
+
+
+
+
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+                dgvResultado.Rows[1].Cells[1].Value = "New Valuev 1";
+                dgvResultado.UpdateCellValue(1, 1);
+
+                dgvResultado.Rows[1].Cells[2].Value = "New Valuev 2";
+                dgvResultado.UpdateCellValue(1, 2);
+*/
+
+            /*
+            DataGridViewRow row = (DataGridViewRow)dgvResultado.RowTemplate.Clone();
+            row.CreateCells(dgvResultado, "HOLA","COMO", "ESTAS");
+            dgvResultado.Rows.Add(row); 
+            */
+
+            //DataGridCell[,] x = new DataGridCell[1,2];
+            //x[0, 0] = "a";
+            //x[0, 1] = "a";
+
+            //DataGridView dgv;
+            //DataGridViewColumn[] dgvColumn = new DataGridViewColumn[2];
+            //DataGridViewRow dgvRow;
+
+
+            //int value;
+            //if(int.TryParse(dgvRow.Cells[1].FormattedValue, out value))
+
+
+            // dgvColumn[1].
+
+            // Value = 100;
+            // dgvColumn[2].Value = 200;
+
+            //CurrentCell.Value = newValue;
+            //DataGridCell x;
+            //x. = "aaaaa";
+
+
+
+            /*
+                XmlNodeList xNodelst = xdoc.DocumentElement.SelectNodes("Quote");
+                foreach (XmlNode xNode in xNodelst)
+                {
+                    sEmisora = xNode["Emisora"].InnerText;
+                }
+            */
+                
+  
+
+
+        }
+ 
+        #endregion
+    }
+}
+
